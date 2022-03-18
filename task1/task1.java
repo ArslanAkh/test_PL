80213f2c8d09ba35965afbac950c9181fc07f187
ArslanAkh <101703961+ArslanAkh@users.noreply.github.com>
2022-03-18 19:20:32 +0300

    Add files via upload

---
+++ task1/task1.java
@@ -0,0 +1,24 @@
+package task1;
+
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+        int c = 1;
+
+        do {
+            System.out.print(c);
+            c = c + m - 1;
+            while (c > n) {
+                c = c - n;
+            }
+        } while (c != 1);
+
+        System.out.println();
+
+        scan.close();
+    }
+}
